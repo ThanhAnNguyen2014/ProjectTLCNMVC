270866e27b4829d9d738b50c83d2cf98603442ea
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2016-11-08 19:01:51 +0700

    Add project files.

---
+++ src/ProjectTLCNMVC/Entities/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectTLCNMVC.Entities
+{
+    public partial class Admin
+    {
+        public int AdminId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int? Role { get; set; }
+    }
+}
